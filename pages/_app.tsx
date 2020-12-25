91215ed7aecfc741b810e458335479f4bf611d64
Hoerin Doh <hellodhlyn@gmail.com>
2020-12-26 02:36:08 +0900

    Adding ga trackings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import NextProgress from 'nextjs-progressbar';
+import { useEffect } from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'tailwindcss/tailwind.css';
 import NavBar from '../components/navbar';
 import Footer from '../components/footer';
+import { pageview } from '../lib/gtag';
 
 const pathsToHideNavBar = ['', '/'];
 const pathsToHideFooter = ['', '/', '/404'];
@@ -12,6 +14,12 @@ const pathsToHideFooter = ['', '/', '/404'];
 function MyApp({ Component, pageProps, router }: AppProps) {
   const hideNavBar = pathsToHideNavBar.includes(router.pathname);
   const hideFooter = pathsToHideFooter.includes(router.pathname);
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => pageview(url);
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
 
   return (
     <>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, {
   Html, Head, Main, NextScript,
 } from 'next/document';
+import { gaTrackingId } from '../lib/gtag';
 
 export default class MyDoucment extends Document {
   static async getInitialProps(ctx) {
@@ -12,6 +13,19 @@ export default class MyDoucment extends Document {
     return (
       <Html>
         <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
           <link href="https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css" rel="stylesheet" type="text/css" />
         </Head>
         <body>
