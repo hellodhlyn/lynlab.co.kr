cf2e99d710a4a97d57d9b547a976e356d474c0d8
Hoerin Doh <hellodhlyn@gmail.com>
2020-12-25 17:49:13 +0900

    Add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import NextProgress from 'nextjs-progressbar';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'tailwindcss/tailwind.css';
 import NavBar from '../components/navbar';
@@ -16,6 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>LYnLab</title>
       </Head>
       <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300 leading-relaxed">
+        <NextProgress color="#339af0" height={5} />
         <NavBar />
         <div className={isIndex ? '' : 'pt-20'}>
           <Component {...pageProps} />
