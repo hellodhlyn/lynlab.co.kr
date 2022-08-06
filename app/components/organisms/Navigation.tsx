92335498b63c89b9290df5b85265baa5bfd6c31d
Hoerin Doh <hellodhlyn@gmail.com>
2022-08-06 15:58:10 +0900

    Add a link to introduce page on the navigation

--- app/components/organisms/Navigation.tsx
+++ app/components/organisms/Navigation.tsx
@@ -1,13 +1,17 @@
 import { Link } from "@remix-run/react";
 import Container from "~/components/atoms/Container";
+import Href from "~/components/atoms/Href";
 
 export default function Navigation() {
   return (
     <Container className="pt-16 pb-8">
       <p className="text-5xl md:text-6xl font-black">LYnLab</p>
       <p className="py-8 text-xl font-bold">
+        <Href link="https://hello.dhlyn.me" underline={false}>
+          <span className="pr-2 hover:opacity-50 transition-opacity">소개</span>
+        </Href>
         <Link to="/blog">
-          <span className="pr-2 hover:opacity-50 transition-opacity">블로그</span>
+          <span className="px-2 hover:opacity-50 transition-opacity">블로그</span>
         </Link>
       </p>
     </Container>
