4b8160de24f296a8d30991c4fffade6c12f366fe
Hoerin Doh <hellodhlyn@gmail.com>
2023-07-16 18:15:35 +0900

    Add features to edit the post with multiple blobs

--- app/components/atoms/inputs/Select.tsx
+++ app/components/atoms/inputs/Select.tsx
@@ -11,10 +11,10 @@ type SelectProps = {
 export function Select({ name, options, label, defaultValue }: SelectProps): JSX.Element {
   return (
     <>
-      {label && <p className="py-4 font-bold">{label}</p>}
+      {label && <p className="my-2 font-bold">{label}</p>}
       <select
         name={name}
-        className="w-full h-12 border-0 bg-white rounded-lg shadow-lg shadow-gray-200"
+        className="w-full my-2 h-12 border-0 bg-white rounded-lg shadow-lg shadow-gray-200"
         defaultValue={defaultValue || options[0].value}
       >
         {options.map((option) => (<option key={option.value} value={option.value}>{option.label}</option>))}
---
+++ app/components/atoms/inputs/index.ts
@@ -0,0 +1,10 @@
+import { Input, useInput } from "./Input";
+import { Select } from "./Select";
+import { Textarea } from "./Textarea";
+
+export {
+  Input,
+  useInput,
+  Select,
+  Textarea,
+};
