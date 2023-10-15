af963bc1e3bbfb0a919d7ff7a3a9781edef191d9
Hoerin Doh <hellodhlyn@gmail.com>
2023-10-15 15:28:11 +0900

    Upgrade dependencies

--- remix.config.js
+++ remix.config.js
@@ -9,13 +9,4 @@ module.exports = {
   server: "./server.js",
 
   devServerBroadcastDelay: 1000,
-
-  // opt-in v2 features
-  future: {
-    v2_headers: true,
-    v2_meta: true,
-    v2_errorBoundary: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  }
 };
