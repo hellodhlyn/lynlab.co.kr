5703c135b4cd6fdf84897931ceae4cd13e866347
Hoerin Doh <hellodhlyn@gmail.com>
2023-11-30 11:23:16 +0000

    Fix that syntax highlighting not works

--- app/components/molecules/blobviews/MarkdownBlob.tsx
+++ app/components/molecules/blobviews/MarkdownBlob.tsx
@@ -1,21 +1,24 @@
-import { marked } from "marked";
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
 
 type MarkdownProps = {
   text: string;
 };
 
 export function MarkdownBlob({ text }: MarkdownProps) {
-  marked.setOptions({
-    highlight: (code, markedLang) => {
-      const hljs = require("highlight.js/lib/common");
-      const language = hljs.getLanguage(markedLang) ? markedLang : "plaintext";
-      return hljs.highlight(code, { language }).value;
-    },
-  });
+  const marked = new Marked(
+    markedHighlight({
+      highlight: (code, lang) => {
+        const hljs = require("highlight.js");
+        const language = hljs.getLanguage(lang) ? lang : "plaintext";
+        return hljs.highlight(code, { language }).value;
+      },
+    }),
+  );
 
   return (
     <div className="prose md:prose-lg max-w-none prose-a:text-blue-500 break-words prose-li:m-0 prose-img:mx-auto">
-      <div dangerouslySetInnerHTML={{ __html: marked(text) }} />
+      <div dangerouslySetInnerHTML={{ __html: marked.parse(text) }} />
     </div>
   );
 }
